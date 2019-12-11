8c9ce6e084b13718869c840f3b8ad06e257403a8
RUI-WEN <rui931224@sina.com>
2019-12-11 19:47:49 +0800

    Site updated: 2019-12-11 19:47:49

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/RUI-WEN/Blog-Back-Up/blob/master/photo/' + data.link[i];
-          var src = 'https://github.com/RUI-WEN/Blog-Back-Up/blob/master/photo/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/RUI-WEN/Blog-Back-Up/master/photo/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/RUI-WEN/Blog-Back-Up/master/photo/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
